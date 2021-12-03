47ed0deb27913c4fc95083e43cf11734ada3413a
long10024070 <long10024070@gmail.com>
2021-12-04 02:32:21 +0700

    create chat&report Controller

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\MessageController;
+
+class ChatController extends MessageController
+{
+    //
+}
