459f54d71d15a67ea330f368fc02b34b05cdafa0
long10024070 <long10024070@gmail.com>
2021-12-16 04:15:28 +0700

    create broadcast for Noti

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -2,17 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Database\Eloquent\BroadcastsEvents;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    use HasFactory;
+    use BroadcastsEvents, HasFactory;
 
     protected $primaryKey = "notification_id";
     protected $fillable = ['title', 'detail', 'status', 'receiver_id'];
 
     public function receiver(){
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function broadcastOn($event)
+    {
+        return new PrivateChannel('NotificationChannel.User.'.$this->receiver_id);
+        // return [new PrivateChannel('NotificationChannel.User.'.$this->receiver_id), new Channel('NotificationChannel.User.'.$this->receiver_id)];
     }
 }
--- resources/views/testlisten.blade.php
+++ resources/views/testlisten.blade.php
@@ -8,17 +8,54 @@
     Pusher.logToConsole = true;
 
     var pusher = new Pusher('a13024e4824fe0c8b79c', {
-      cluster: 'ap1'
+      cluster: 'ap1',
+      forceTLS: true,
+      authEndpoint: "/broadcasting/auth",
+      auth: {
+          headers: {
+            Authorization: 'Bearer ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9hdXRoXC9sb2dpbiIsImlhdCI6MTYzOTIyOTgxNywiZXhwIjoxNjM5ODM0NjE3LCJuYmYiOjE2MzkyMjk4MTcsImp0aSI6IlZGcDRUUlJFaGhOWGFjdTAiLCJzdWIiOjEsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.qRA0AwUGmW1xMEn-_JtXxmnbZ8Ox6fqeJfiRc17YO14'
+          }, // Access Token cua User co user_id = 1
+      },
     });
 
-    var channel = pusher.subscribe('MessageChannel.User.10');
-    channel.bind('MessageUpdated', function(data) {
+    var channel1 = pusher.subscribe('MessageChannel.User.1');
+    channel1.bind('MessageUpdated', function(data) {
       alert(JSON.stringify(data));
-      console.log("Update");
+      console.log("User 1 get Update");
     });
-    channel.bind('MessageCreated', function(data) {
+    channel1.bind('MessageCreated', function(data) {
       alert("Create"+JSON.stringify(data));
-      console.log("Create");
+      console.log("User 1 get Create");
+    });
+
+    var channel2 = pusher.subscribe('private-MessageChannel.User.1');
+    channel2.bind('MessageUpdated', function(data) {
+      alert(JSON.stringify(data));
+      console.log("User 1 get PRIVATE Update");
+    });
+    channel2.bind('MessageCreated', function(data) {
+      alert("Create"+JSON.stringify(data));
+      console.log("User 1 get PRIVATE Create");
+    });
+
+    var channel3 = pusher.subscribe('MessageChannel.User.2');
+    channel3.bind('MessageUpdated', function(data) {
+      alert(JSON.stringify(data));
+      console.log("User 2 get Update");
+    });
+    channel3.bind('MessageCreated', function(data) {
+      alert("Create"+JSON.stringify(data));
+      console.log("User 2 get Create");
+    });
+
+    var channel4 = pusher.subscribe('private-MessageChannel.User.2');
+    channel4.bind('MessageUpdated', function(data) {
+      alert(JSON.stringify(data));
+      console.log("User 2 get PRIVATE Update");
+    });
+    channel4.bind('MessageCreated', function(data) {
+      alert("Create"+JSON.stringify(data));
+      console.log("User 2 get PRIVATE Create");
     });
   </script>
 </head>
