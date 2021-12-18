597bb1d1c7507e114ce6d4aabe356934fea58c0e
long10024070 <long10024070@gmail.com>
2021-12-18 15:19:05 +0700

    fixed broadcast bug

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,7 +38,6 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
-                'scheme' => 'http'
             ],
         ],
 
