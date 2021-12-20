93b82145b4e6f9b5d3240edb45cbdc3e6034f125
long10024070 <long10024070@gmail.com>
2021-12-20 21:35:37 +0700

    change report to review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,12 +9,8 @@ class Review extends Model
 {
     use HasFactory;
 
-<<<<<<< HEAD:app/Models/Review.php
 
     protected $primaryKey = 'review_id';
-=======
-    protected $primaryKey = 'report_id';
->>>>>>> f46544b38b576042a1804d6c02a4ac1a3d026b64:app/Models/Report.php
     protected $fillable = ['detail', 'status', 'sender_id', 'receiver_id'];
 
     public function sender() {
